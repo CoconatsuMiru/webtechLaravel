d0dc93aae921c5aa82f5ff4518cfee95b2e3e613
ArAr <hhfrostie@gmail.com>
2025-02-13 15:23:17 +0800

    added a few things in add form

--- resources/views/create/create.blade.php
+++ resources/views/create/create.blade.php
@@ -8,42 +8,45 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/createStyle.css">
 </head>
-<div class="container">
-  <div class="formCenter">
-    <div class="formBorder">
-      <form>
-        <button type="submit" class="btn btn-back mb-4">Back</button>
-        <div class="mb-3">
-          <label for="categorySelect" class="form-label">Categories:</label>
-          <select class="form-select" id="categorySelect">
-            <option selected>Choose a category</option>
-            <option value="1">Technology</option>
-            <option value="2">Health</option>
-            <option value="3">Education</option>
-            <option value="4">Finance</option>
-          </select>
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Item Name:</label>
-          <input type="text" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Price:</label>
-          <input type="text" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Quantity:</label>
-          <input type="text" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="row">
-          <div class="col text-end">
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+<body>
+  <div class="container">
+    <div class="formCenter">
+      <div class="formBorder">
+        <form>
+          <button type="submit" class="btn btn-back mb-3">Back</button>
+          <h1 class="welcome text-center">Welcome!</h1>
+          <div class="mb-3">
+            <label for="categorySelect" class="form-label">Categories:</label>
+            <select class="form-select" id="categorySelect">
+              <option selected>Choose a category</option>
+              <option value="1">Technology</option>
+              <option value="2">Health</option>
+              <option value="3">Education</option>
+              <option value="4">Finance</option>
+            </select>
           </div>
-        </div>
-      </form>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Item Name:</label>
+            <input type="text" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Price:</label>
+            <input type="text" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Quantity:</label>
+            <input type="text" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="row">
+            <div class="col text-end">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-</div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
