16f5d1eefa142384741e784b64151c43c05b2e04
ArAr <hhfrostie@gmail.com>
2025-02-24 21:13:38 +0800

    Feat: Making page responsive and Integrating Bootstrap

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -6,36 +6,50 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-  <title>Document</title>
+  <title>Responsive Table</title>
 </head>
 
 <body>
-  <div class="row">
-    <div class="col">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">IGN</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Rafael</td>
-            <td>Rafols</td>
-            <td>Koffi</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Wilkins</td>
-            <td>Castro</td>
-            <td>Lemon</td>
-          </tr>
-        </tbody>
-      </table>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-8 col-lg-6 col-xl-5">
+        <table class="table table-striped table-responsive">
+          <thead class="table-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">IGN</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Rafael</td>
+              <td>Rafols</td>
+              <td>Koffi</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Wilkins</td>
+              <td>Castro</td>
+              <td>Lemon</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Rhenzal</td>
+              <td>Uy</td>
+              <td>Yamjago</td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>Aldec</td>
+              <td>Permison</td>
+              <td>Bubble</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </body>
